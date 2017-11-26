49409395f7879d2db4a54e01ec3ee26829f8984e
voidSatisfaction <lourie@naver.com>
2017-11-27 01:40:25 +0900

    feat: add solution of 1102

---
+++ 1102.cpp
@@ -0,0 +1,69 @@
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+
+#define MAX_N 16
+#define INF 987654321
+
+int N, cost[MAX_N][MAX_N], cache[MAX_N][1 << MAX_N];
+int visited, visitedNum, P;
+
+int getMinCost(int visitedNum, int visited)
+{
+  if (visitedNum >= P){
+    return 0;
+  }
+
+  int& result = cache[visitedNum][visited];
+  if (result != -1){
+    return result;
+  }
+
+  result = INF;
+  for (int i = 0; i < N; i++)
+  {
+    if ((visited & (1 << i)) == 0) continue;
+    int cur = i;
+    for (int j = 0; j < N; j++)
+    {
+      if (i == j || (visited & (1 << j))) continue;
+      int next = j;
+      result = std::min(
+        result,
+        getMinCost(visitedNum + 1, (visited | (1 << next))) + cost[cur][next]
+      );
+    }
+  }
+  return result;
+}
+
+int main()
+{
+  scanf("%d", &N);
+  for (int i = 0; i < N; i++)
+    for (int j = 0; j < N; j++)
+      scanf("%d", &cost[i][j]);
+
+  for (int i = 0; i < N; i++)
+  {
+    char c;
+    scanf("\n%c", &c);
+    if (c == 'Y'){
+      visited |= 1 << i;
+      visitedNum++;
+    }
+  }
+
+  std::memset(cache, -1, sizeof(cache));
+
+  scanf("%d", &P);
+
+  int minCost = getMinCost(visitedNum, visited);
+  if (minCost == INF){
+    printf("%d\n", -1);
+  } else {
+    printf("%d\n", minCost);
+  }
+
+  return 0;
+}
